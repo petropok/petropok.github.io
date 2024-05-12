3dfe16a9c98178faa8a4e022535e7f6a4112c855
petropok <mimogios@gmail.com>
2024-05-13 01:19:30 +0300

    Added navbar

--- main.js
+++ main.js
@@ -1,28 +1,38 @@
 import './style.css'
 
 import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+if ( WebGL.isWebGLAvailable() ) {
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+	const scene = new THREE.Scene();
+	const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+	const renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
 
-camera.position.z = 5;
+	const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+	const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+	const cube = new THREE.Mesh( geometry, material );
+	scene.background = new THREE.Color( 0xb233ff );
+	scene.add( cube );
 
-function animate() {
-	requestAnimationFrame( animate );
+	camera.position.z = 5;
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	function animate() {
+		requestAnimationFrame( animate );
 
-	renderer.render( scene, camera );
+		cube.rotation.x += 0.01;
+		cube.rotation.y += 0.01;
+
+		renderer.render( scene, camera );
+	}
+
+	animate();
+} else {
+
+	const warning = WebGL.getWebGLErrorMessage();
+	document.getElementById( 'container' ).appendChild( warning );
+
 }
-
-animate();
