1e37ba914c2db7960f896996991cc8f68cd5a4a9
petropok <mimogios@gmail.com>
2024-08-21 00:25:30 +0300

    update

--- main.js
+++ main.js
@@ -3,19 +3,21 @@ import './style.css'
 import * as THREE from 'three';
 import WebGL from 'three/addons/capabilities/WebGL.js';
 
+
 if ( WebGL.isWebGLAvailable() ) {
+	var canvReference = document.getElementById("ship-canvas");
+	const renderer = new THREE.WebGLRenderer({
+		antialias:true,
+		canvas: canvReference
+	});
 
 	const scene = new THREE.Scene();
 	const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-	const renderer = new THREE.WebGLRenderer();
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	document.body.appendChild( renderer.domElement );
-
 	const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 	const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 	const cube = new THREE.Mesh( geometry, material );
-	scene.background = new THREE.Color( 0xb233ff );
+	scene.background = new THREE.Color( 0x3F51B5 );
 	scene.add( cube );
 
 	camera.position.z = 5;
